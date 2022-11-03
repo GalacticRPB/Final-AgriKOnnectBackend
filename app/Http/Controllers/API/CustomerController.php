7f02cb3c61385803ff51f7aa3cde2ca90945f0fb
Marian Gem Bermudez <mariangembermudez1207@gmail.com>
2022-11-04 00:56:44 +0800

    Customer Account

---
+++ app/Http/Controllers/API/CustomerController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\API;
+use App\Models\Customer;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class CustomerController extends Controller
+{
+    //
+    function registerCustomer(Request $req)
+    {
+        $customer = new Customer;
+        $customer->firstname=$req->input('firstname');
+        $customer->middlename=$req->input('middlename');
+        $customer->lastname=$req->input('lastname');
+        $customer->username=$req->input('username');
+        $customer->email=$req->input('email');
+        $customer->password=Hash::make($req->input('password'));
+        $customer->save();
+        return $customer->toJson();
+    }
+
+    //
+    function loginCustomer(Request $req)
+    {
+        $customer= Customer::where('username',$req->username)->first();
+        if(!$customer || !Hash::check($req->password, $customer->password))
+        {
+            return ["error"=> "Email or password is incorrect"];
+        }
+        return $customer;
+    }
+
+    public function editCustomer($id)
+    {
+        $customer = Customer::find($id);
+        if($customer)
+        {
+            return response()->json([
+                'status'=> 200,
+                'customer' => $customer,
+            ]);
+        }
+        else
+        {
+            return response()->json([
+                'status'=> 404,
+                'message' => 'No Customer ID Found',
+            ]);
+        }
+
+    }
+
+    public function updateCustomer(Request $req, $id)
+    {
+        $validator = Validator::make($req->all(),[
+            'firstname'=>'nullable',
+            'middlename'=>'nullable',
+            'lastname'=>'nullable',
+            'mobilephone'=>'required|max:11',
+            'email'=>'nullable',
+            'password'=>'nullable|min:8',
+            'address'=>'required',
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json([
+                'status'=> 422,
+                'validationErrors'=> $validator->messages(),
+            ]);
+        }
+        else
+        {
+            $customer = Customer::find($id);
+            if($customer)
+            {
+                $customer->firstname=$req->input('firstname');
+                $customer->middlename=$req->input('middlename');
+                $customer->lastname=$req->input('lastname');
+                $customer->mobilephone=$req->input('mobilephone');
+                $customer->email=$req->input('email');
+                $customer->password=Hash::make($req->input('password'));
+                $customer->address=$req->input('address');
+                $customer->update();
+
+                return response()->json([
+                    'status'=> 200,
+                    'message'=>'Customer Account Updated Successfully',
+                ]);
+            }
+            else
+            {
+                return response()->json([
+                    'status'=> 404,
+                    'message' => 'No Customer ID Found',
+                ]);
+            }
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\CustomerController;
 use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//seller
+
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
 Route::get('/edit/{id}', [UserController::class, 'edit']);
@@ -26,6 +29,13 @@ Route::put('/update/{id}', [UserController::class, 'update']);
 Route::get('/editPassword/{id}', [UserController::class, 'edit']);
 Route::put('/updatePassword/{id}', [UserController::class, 'updatePassword']);
 
+//customer
+Route::post('registerCustomer', [CustomerController::class, 'registerCustomer']);
+Route::post('loginCustomer', [CustomerController::class, 'loginCustomer']);
+Route::get('/editCustomer/{id}', [CustomerController::class, 'editCustomer']);
+Route::put('/updateCustomer/{id}', [CustomerController::class, 'updateCustomer']);
+Route::get('/editCustomerPassword/{id}', [CustomerController::class, 'edit']);
+Route::put('/updateCustomerPassword/{id}', [CustomerController::class, 'updateCustomerPassword']);
 
 //Route::get('list', [ProductController::class, 'list']);
 Route::get('products/{user_id}', [ProductController::class, 'getUserProducts']);
